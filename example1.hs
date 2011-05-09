0f9e6bfe4b41efee173ba783f9124980259ef205
Dominic Orchard <dom.orchard@gmail.com>
2011-05-09 18:24:19 +0100

    example1 added

--- example1.hs
+++ example1.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 module Example1 where
@@ -7,19 +8,14 @@ module Example1 where
 import Ypnos
 import Ypnos.Core.Grid
 
-import Prelude hiding (iterate)
-
 dat = [1,2,3]::[Int]
 
-grid1 = listGrid (Dim X) 0 3 dat
+datBound = [boundary| Int -1 -> 0
+                          +1 -> 0 |]
 
-f = [$fun| X:| a @b c | -> a+b+c |]
+grid1 = listGridWithBoundaries (Dim X) 0 3 dat datBound
+
+f = [fun| X:| a @b c | -> a+b+c |]
 
 grid1' = run f grid1
-
---converge = reducer (+) (+) 0 (\x -> x > 100)
-
---grid1'' = iterate f converge (defaults grid1 0)
-
--- result show be elems = [(Ix[0],57),(Ix[1],82),(Ix[2],59)]
 
