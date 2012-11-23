71e4a684a67140771dbe805408129701d0f17c5c
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2012-11-23 00:41:00 +0000

    Implemented the 1D avg function using the new Ypnos fun compilation.

--- experiments/Average.hs
+++ experiments/Average.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, QuasiQuotes #-}
 module Average (runAvg) where 
 
 import Prelude hiding (zipWith)
+import Ypnos hiding (fun, run)
+import Ypnos.CUDA.Expr.Fun 
 import Data.Array.Accelerate hiding (flatten)
 import Data.Array.Accelerate.Interpreter 
 
@@ -14,3 +16,16 @@ runAvg :: (IsFloating a, Elt a) => [a] -> Int -> Int -> [a]
 runAvg xs x y = toList $ run (stencil avg Clamp acc_xs)
     where acc_xs = use xs'
           xs' = fromList (Z :. x :. y) xs
+          
+-- Why does this not work?
+-- avg' = [fun| Z:|a @b c| -> a + b + c|]:: Floating (Exp a) => Stencil3 a -> 
+-- Exp a
+
+avg' :: Floating (Exp a) => Stencil3 a -> Exp a
+avg' = [fun| Z:|a @b c| -> (a + b + c)/3|]
+
+-- TODO: implement the Ypnos function for run
+runAvg' :: (IsFloating a, Elt a) => [a] -> Int -> [a]
+runAvg' xs x = toList $ run (stencil avg' Clamp acc_xs)
+    where acc_xs = use xs'
+          xs' = fromList (Z :. x) xs
