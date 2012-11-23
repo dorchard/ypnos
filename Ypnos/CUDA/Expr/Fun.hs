18897550ce995c34f7b70af685aa93e27f8a98ec
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2012-11-23 00:40:13 +0000

    Implemented a basic compiler for 1D stencils. Mostly works by having pilfered most of the code for interpret. The changes are that now we don't take a grid but the lambda contains a stencil formatted argument as per the Accelerate Stencil requirement.

--- Ypnos/CUDA/Expr/Fun.hs
+++ Ypnos/CUDA/Expr/Fun.hs
@@ -8,6 +8,7 @@ import Ypnos.Core.Grid
 import Data.Maybe
 import Data.Generics
 import Language.Haskell.TH 
+import Language.Haskell.TH.Syntax
 import Language.Haskell.TH.Quote
 import Language.Haskell.Meta.Parse
 import Ypnos.Expr.Expr
@@ -30,7 +31,7 @@ quoteExprExp input = do loc <- location
                                fst (loc_start loc),
                                snd (loc_start loc))
                         expr <- (parseExpr gridFun) pos input
-                        dataToExpQ (const Nothing) expr
+                        dataToExpQ (const Nothing `extQ` interpret) expr
 
 
 quoteExprPat :: String -> PatQ
@@ -40,3 +41,32 @@ quoteExprPat input = do loc <- location
                                snd (loc_start loc))
                         expr <- (parseExpr gridFun) pos input
                         dataToPatQ (const Nothing) expr
+
+interpret :: GridFun -> Maybe (Q Exp)
+interpret (GridFun pattern body) = 
+    case parseExp body of
+      Left x -> error x
+      Right bodyExpr -> Just gridFun
+          where
+            gridFun = lamE [gpat] (return bodyExpr)
+            gpat = mkPattern pattern  
+
+mkPattern :: GridPattern -> PatQ
+mkPattern (GridPattern1D dim vars) =
+    tupP pats
+    where 
+    pats = mkVars vars
+
+mkVars :: [VarP] -> [PatQ]
+mkVars vs = map getName vs
+
+getName :: VarP -> PatQ
+getName v = 
+    case uncurse v of
+      PatternVar x -> varP $ mkName x
+      PatternBlank -> wildP
+
+    
+uncurse :: VarP -> VarP'
+uncurse (Cursor v) = v
+uncurse (NonCursor v) = v
