8179d475bc9f192b4350b7df844be04dcffd1eb8
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2012-12-18 23:33:16 +0400

    Previous approach to the centering the patterns was bottom up and getting messy. Now trying a top down approach which seems to be going a lot better. Lots of code is currenty commented out as it has been broken.
    Had to use an intermediary type family called GridPattern' to bring the
    dimentionality of the pattern into the type system. This allows us to
    better deal with the different index types required when converting into
    the intermediary representation.
    
    The plan is to use arrays for the intermediary then write some functions
    that deal with the insertion of columns and centering of cursor. Then it
    will be trivial to convert this into the tuple representation.

--- Ypnos/CUDA/Expr/Fun.hs
+++ Ypnos/CUDA/Expr/Fun.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Ypnos.CUDA.Expr.Fun where
 
@@ -12,26 +17,28 @@ import Language.Haskell.TH.Syntax
 import Language.Haskell.TH.Quote
 import Language.Haskell.Meta.Parse
 import Ypnos.Expr.Expr
+import Data.Ix
+import Data.Array.IArray
 
 import Debug.Trace
 
 fvar = varE . mkName
 fcon = conE . mkName
 
-fun :: QuasiQuoter
-fun = QuasiQuoter { quoteExp = quoteExprExp,
-                    quotePat = quoteExprPat,
-                    quoteType = undefined,
-                    quoteDec = undefined
-                  }
+--fun :: QuasiQuoter
+--fun = QuasiQuoter { quoteExp = quoteExprExp,
+--                   quotePat = quoteExprPat,
+--                   quoteType = undefined,
+--                   quoteDec = undefined
+--                 }
 
-quoteExprExp :: String -> ExpQ
-quoteExprExp input = do loc <- location
-                        let pos = (loc_filename loc,
-                               fst (loc_start loc),
-                               snd (loc_start loc))
-                        expr <- (parseExpr gridFun) pos input
-                        dataToExpQ (const Nothing `extQ` interpret) expr
+--quoteExprExp :: String -> ExpQ
+--quoteExprExp input = do loc <- location
+--                        let pos = (loc_filename loc,
+--                               fst (loc_start loc),
+--                               snd (loc_start loc))
+--                        expr <- (parseExpr gridFun) pos input
+--                        dataToExpQ (const Nothing `extQ` interpret) expr
 
 
 quoteExprPat :: String -> PatQ
@@ -42,21 +49,56 @@ quoteExprPat input = do loc <- location
                         expr <- (parseExpr gridFun) pos input
                         dataToPatQ (const Nothing) expr
 
-interpret :: GridFun -> Maybe (Q Exp)
-interpret (GridFun pattern body) = 
-    case parseExp body of
-      Left x -> error x
-      Right bodyExpr -> Just gridFun
-          where
-            gridFun = lamE [gpat] (return bodyExpr)
-            gpat = mkPattern pattern  
+--interpret :: GridFun -> Maybe (Q Exp)
+--interpret (GridFun pattern body) = 
+--    case parseExp body of
+--      Left x -> error x
+--      Right bodyExpr -> Just gridFun
+--          where
+--            gridFun = lamE [gpat] (return bodyExpr)
+--            gpat = mkPattern pattern  
 
-mkPattern :: GridPattern -> PatQ
-mkPattern pat = 
-    case pat of
-        (GridPattern1D _ _) -> mkPattern1D pat
-        (GridPattern2D _ _ vars) -> tupP (map pkgPattern vars)
+--mkPattern :: GridPattern -> PatQ
+--mkPattern = intToPatt . centreCursor . ypToInt
+
+data Intermediate i where 
+    Inter :: Ix i => i -> Array i VarP -> Intermediate i
+
+data family GridPattern' d 
+data instance GridPattern' (Dim x) = 
+    GridPattern1D' DimTag [VarP]
+data instance GridPattern' (Dim x :* Dim y) = 
+    GridPattern2D' DimTag DimTag [[VarP]]
+
+ypToInt :: GridIx d i => GridPattern' d -> Intermediate i
+ypToInt pat = 
+        let range = getBounds pat 
+            ls = safeConcat pat 
+        in
+        Inter (getCurIx pat) 
+              (listArray range ls)
+
+class (Ix i, Index d ~ i) => GridIx d i where
+    getCurIx :: GridPattern' d -> i
+    getBounds :: GridPattern' d -> (i, i)
+    safeConcat :: GridPattern' d -> [VarP]
+
+instance GridIx (Dim x) Int where
+    getBounds (GridPattern1D' _ ls) = 
+        (0::Int, length ls)
+    getCurIx (GridPattern1D' _ _) = 1
+    safeConcat (GridPattern1D' _ ls) = ls
+
+instance GridIx (Dim x :* Dim y) (Int,Int) where
+    getBounds (GridPattern2D' _ _ ls) = 
+        ((0::Int,0::Int), (length (head ls), length ls))
+    getCurIx (GridPattern2D' _ _ _) = (1,1)
+    safeConcat (GridPattern2D' _ _ ls) = concat ls
     
+
+--centreCursor :: Intermediate -> Intermediate
+--intToPatt :: Intermediate -> PatQ
+
 pkgPattern :: [VarP] -> PatQ
 pkgPattern xs = tupP (mkVars xs)
 
@@ -73,52 +115,6 @@ uncurse :: VarP -> VarP'
 uncurse (Cursor v) = v
 uncurse (NonCursor v) = v
 
-mkPattern1D :: GridPattern -> PatQ
-mkPattern1D pat = tupP (mkPattern1D' (fromJust (getCursorLocation pat [0])) (getNonCursorLocations pat [0]) 0)
-
-mkPattern1D' :: Position -> [Position] -> Int -> [PatQ]
-mkPattern1D' p (n:ncs) x | l==x = (getName' c):(mkPattern1D' p (n:ncs) (x+1))
-                      | ln==x = (getName' nc):(mkPattern1D' p ncs (x+1))
-                      | x > 2*l = []
-                      | otherwise = wildP:(mkPattern1D' p ncs (x+1)) --TODO: avoid this repetition
-    where Position nc (ln:_) = n 
-          Position c (l:_) = p
-mkPattern1D' p _ x = [] --TODO: deal with case of cursor being last element
-
 getName' :: VarP' -> PatQ
 getName' (PatternVar x) = varP $ mkName x
 getName' PatternBlank = wildP
-
-data Position = Position VarP' [Int] deriving Show
-
-getCursorLocation :: GridPattern -> [Int] -> Maybe Position
-getCursorLocation (GridPattern1D x ((Cursor v):vs)) loc = Just (Position v loc)
-getCursorLocation (GridPattern1D x ((NonCursor _):vs)) (l:ls) = getCursorLocation (GridPattern1D x vs) ((l+1):ls)
-getCursorLocation (GridPattern1D x []) _ = Nothing
-getCursorLocation (GridPattern2D x y (v:vs)) (l:ls) = 
-                case getCursorLocation (GridPattern1D x v) (0:l:ls) of
-                    Just a -> Just a
-                    Nothing -> getCursorLocation (GridPattern2D x y vs) ((l+1):ls)
-getCursorLocation (GridPattern2D x y []) _ = Nothing
-
-getNonCursorLocations (GridPattern1D x curs) ls = 
-        case curs of
-            (v:vs) ->
-                let next = getNonCursorLocations (GridPattern1D x vs) ((l+1):locs)
-                    (l:locs) = ls
-                in case v of
-                    (Cursor _) -> next
-                    (NonCursor v) -> (Position v ls):next
-            [] -> []
-getNonCursorLocations (GridPattern2D x y curs) ls =
-        case curs of
-            (v:vs) ->
-                let current = getNonCursorLocations (GridPattern1D x v) (0:ls)
-                    (l:locs) = ls
-                    next = getNonCursorLocations (GridPattern2D x y vs) (l+1:locs)
-                in current ++ next
-            [] -> []
-
-getSize :: GridPattern -> [Int]
-getSize (GridPattern1D _ vs) = [length vs]
-getSize (GridPattern2D x _ (v:vs)) = (length (v:vs)):(getSize (GridPattern1D x v)) 
