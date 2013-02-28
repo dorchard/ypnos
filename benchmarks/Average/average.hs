c82f207b4e2812f1d18019fcf28ded2ef6fc0cf6
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2013-02-28 18:21:59 +0000

    Flattened the file hierarchy of the benchmark folder.

--- benchmarks/Average/average.hs
+++
@@ -1,8 +0,0 @@
-import System.Environment
-
-main = do args <- getArgs
-          if length args /= 2 then
-            do print "Wrong number of arguments."
-          else do let [arg1, arg2] = args
-                  print arg1
-                  print arg2
