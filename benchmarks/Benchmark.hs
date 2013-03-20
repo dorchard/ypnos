60002c97d911c05cd92c0db60656fa18458d5079
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2013-03-20 19:16:07 +0000

    Added life to benchmarks.

--- benchmarks/Benchmark.hs
+++ benchmarks/Benchmark.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE GADTs #-}
+
 module Benchmark where
 
 import System.Environment
 
-import Ypnos.Examples.Stencils (runAvgY, runAvg, runAvgY', raiseToList)
+import Ypnos.Examples.Stencils
+  (runAvgY, runAvg, runAvgY', raiseToList, runLife, runLife')
 
 import Criterion
 import Criterion.Monad
@@ -13,11 +16,15 @@ import Data.Vector.Unboxed hiding (map, mapM, foldr, foldr1, (++))
 import Prelude hiding (sum, length)
 import Control.Monad.Trans
 
-l = [1,2,3,4,5,6,7,8,9,10]
+l  = [1,2,3,4,5,6,7,8,9,10]
+l2 = [True, False, False, True, False, True]
 
-avgY = raiseToList runAvgY l
-avgYorig = runAvgY' l
+avgGPU = runAvgY l
+avgCPU = runAvgY' l
 avgA = raiseToList runAvg l
+
+lifeCPU = runLife' l2
+lifeGPU = runLife  l2
 
 stenBench :: ((Int,Int) -> b) -> [Benchmark]
 stenBench f = [ bench "10x10" $ whnf f (10,10)
@@ -29,12 +36,12 @@ stenBench f = [ bench "10x10" $ whnf f (10,10)
 --main = defaultMain [ bgroup "Ypnos" (stenBench runAvgY')
 --                   , bgroup "Accelerate" (stenBench runAvgA')
 --                   ]
-runB :: ((Int,Int) -> b) -> Int -> IO Double
-runB f x = let v = do env <- measureEnvironment
-                      l <- runBenchmark env (whnf f (x,x))
-                      s <- liftIO $ analyseSample 0.5 l 5
-                      m <- analyseMean l 5
-                      return m
+runB :: Fun -> Int -> IO Double
+runB (Fun f) x = let v = do env <- measureEnvironment
+                            l <- runBenchmark env (whnf f (x,x))
+                            s <- liftIO $ analyseSample 0.5 l 5
+                            m <- analyseMean l 5
+                            return m
 
          in  withConfig defaultConfig v
 
@@ -51,13 +58,20 @@ makeLine xs = foldr1 (insert ", ") xs
 printCSV :: [[String]] -> String
 printCSV lls = foldr (insert "\n") "" (map makeLine lls)
 
+type FunS = String
+type ImplS = String
+
+data Fun where
+  Fun :: ((Int,Int) -> b) -> Fun
+
+getFun :: FunS -> ImplS -> Fun
+getFun "avg" "gpu" = Fun avgGPU
+getFun "avg" "cpu" = Fun avgCPU
+getFun "life" "gpu" = Fun lifeGPU
+getFun "life" "cpu" = Fun lifeCPU
+
 main = do [function, impl, begin, step, end, filename] <- getArgs
-          let fun = case function of
-                      "avg" -> case impl of
-                                 "gpu" -> avgY
-                                 "cpu" -> avgYorig
-                                 _ -> error ("Not a know implementation.")
-                      _ -> error ("Not a know function.")
+          let fun = getFun function impl
           let b = read begin :: Int
           let s = read step :: Int
           let e = read end :: Int
