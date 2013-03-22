9b7fda0b7447ee9c6169a38fdb20089f814483f9
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2013-03-22 17:39:18 +0000

    Added lower and upper bound calculation to benchmark.

--- benchmarks/Benchmark.hs
+++ benchmarks/Benchmark.hs
@@ -39,20 +39,25 @@ stenBench f = [ bench "10x10" $ whnf f (10,10)
 --main = defaultMain [ bgroup "Ypnos" (stenBench runAvgY')
 --                   , bgroup "Accelerate" (stenBench runAvgA')
 --                   ]
-runB :: Fun -> Int -> IO Double
+type Result = (Double, Double, Double)
+
+runB :: Fun -> Int -> IO Result
 runB (Fun f) x = let v = do env <- measureEnvironment
                             l <- runBenchmark env (whnf f (x,x))
                             cfg <- getConfig
                             let conf = fromLJ cfgConfInterval cfg
                             let res  = fromLJ cfgResamples cfg
                             s <- liftIO $ analyseSample conf l res
-                            return (estPoint $ anMean s)
+                            let m = anMean s
+                            return (estPoint m,
+                                    estLowerBound m,
+                                    estUpperBound m)
 
          in  withConfig defaultConfig v
 
-makeSet :: (Monad m, Show a) => (Int -> m a) -> [Int] -> m [[String]]
-makeSet f range = let tup x = do y <- f x
-                                 return [show x,show y]
+makeSet :: (Int -> IO Result) -> [Int] -> IO [[String]]
+makeSet f range = let tup x = do (y, l, u)  <- f x
+                                 return [show x, show y, show l, show u]
                   in  mapM tup range
 
 insert i x y = x ++ i ++ y
